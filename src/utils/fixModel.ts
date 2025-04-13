7211e4a9bdc3e5762e6851e36484c861b8190fbc
Jack Le Hamster <jacklehamster@gmail.com>
2025-04-12 18:00:24 -0700

    AI models

--- src/utils/fixModel.ts
+++ src/utils/fixModel.ts
@@ -1,14 +1,24 @@
+const MINIMAL_AI_MODELS = {
+  DEEPSEEK: "deepseek-chat",
+  OPENAI: "gpt-4o-mini",
+  GEMINI: "gemini-2.0-flash-lite",
+  GROK: "grok-3-mini",
+};
+
 export function fixModel(model: string) {
   switch (model) {
+    case "random":
+      const models = Object.values(MINIMAL_AI_MODELS);
+      return models[Math.floor(Math.random() * models.length)];
     case "deepseek":
-      return "deepseek-chat";
+      return MINIMAL_AI_MODELS.DEEPSEEK;
     case "openai":
     case "gpt":
-      return "gpt-4o-mini";
+      return MINIMAL_AI_MODELS.OPENAI;
     case "gemini":
-      return "gemini-2.0-flash-lite";
+      return MINIMAL_AI_MODELS.GEMINI;
     case "grok":
-      return "grok-3-mini";
+      return MINIMAL_AI_MODELS.GROK;
   }
   return model;
 }
